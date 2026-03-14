b5d703f980750e43af54d0b2fd14a995e2c030aa
abdul6teen-max <abdul6teen@gmail.com>
2026-03-14 14:43:05 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,7 +8,20 @@ self.addEventListener('install', e => {
 });
 
 self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());const CACHE = 'mnn-v1';
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
   e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => new Response('')));
+});
+
 });
 
 self.addEventListener('fetch', e => {
